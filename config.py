b5b3772a792233b0f3f1d07f8ff072ee82c6c46c
User <housechel@mail.ru>
2025-04-17 15:48:27 +0300

    Update file config

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    # База данных
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
@@ -10,10 +9,6 @@ class Settings(BaseSettings):
     DB_NAME: str
 
     DATABASE_URL: str
-    # JWT
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
         env_file = ".env"
